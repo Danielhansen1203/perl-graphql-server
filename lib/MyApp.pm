150900fbb10c0c420fb7adc5569646b6625eb51f
Daniel Hansen <danielhansen1203@msn.com>
2025-04-29 01:29:22 +0200

    Godnat nu

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -4,17 +4,25 @@ use Mojo::Base 'Mojolicious';
 sub startup {
     my $self = shift;
 
+    # Fortæl serveren at den skal lytte på 0.0.0.0:3000
+    $self->config(
+        hypnotoad => {
+            listen => ['http://*:3000'],
+            workers => 2,
+            proxy   => 1,
+        }
+    );
+
+    # Routes
     my $r = $self->routes;
 
-    # ROUTES
-
-    # Simple default GET route (viser serveren kører)
+    # Simple GET route
     $r->get('/')->to(cb => sub {
         my $c = shift;
         $c->render(text => 'GraphQL server is running!');
     });
 
-    # GraphQL endpoint POST /graphql
+    # GraphQL endpoint
     $r->post('/graphql')->to('graphql#graphql');
 }
 
