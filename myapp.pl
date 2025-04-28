0d5c5e97b7c05bc4d5937a7a7fe6dd66d4c7e460
Daniel Hansen <danielhansen1203@msn.com>
2025-04-29 00:40:27 +0200

    myapp.pl change

--- myapp.pl
+++ myapp.pl
@@ -1,42 +1,8 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Mojolicious::Lite;
-use Mojolicious::Plugin::GraphQL;
-use GraphQL::Schema;
-use GraphQL::Type::Object;
-use GraphQL::Type::Scalar;
+use lib 'lib';
+use MyApp;
 
-# Definér en simpel String-type
-my $StringType = GraphQL::Type::Scalar->new(
-  name => 'String',
-  serialize => sub { $_[0] },
-);
-
-# Definér root Query
-my $query_root = GraphQL::Type::Object->new(
-  name   => 'Query',
-  fields => {
-    hello => {
-      type    => $StringType,
-      resolve => sub { 'Hello from Perl GraphQL!' },
-    },
-    Abkat => {
-      type    => $StringType,
-      resolve => sub { 'DET SPILLER BAR!' }, 
-    },
-  },
-);
-
-# Lav et GraphQL schema
-my $schema = GraphQL::Schema->new(
-  query => $query_root,
-);
-
-# Tilføj plugin OG sig at det selv skal lave route!
-plugin GraphQL => { schema => $schema };
-
-# Test GET route
-get '/' => { text => 'GraphQL server is AaDrunning!' };
-
-app->start;
+# Start Mojolicious app
+MyApp->new->start;
