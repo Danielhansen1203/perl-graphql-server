88e0c67efbb5e8434912a555349857dd7b72029c
Daniel Hansen <danielhansen1203@msn.com>
2025-04-29 02:07:51 +0200

    schema 0.54

--- lib/MyApp/Schema/Graphql.pm
+++ lib/MyApp/Schema/Graphql.pm
@@ -2,19 +2,44 @@ package MyApp::Schema::Graphql;
 use strict;
 use warnings;
 use GraphQL::Schema;
+use GraphQL::Type::Object;
+use GraphQL::Type::Scalar;
 
 sub schema {
-    return GraphQL::Schema->from_doc(<<'GRAPHQL', {
-        hello => sub { "Hello World" },
-    });
+    my ($snmp_model) = @_;
+
+    my $Query = GraphQL::Type::Object->new(
+        name => 'Query',
+        fields => {
+            temperature => {
+                type => GraphQL::Type::Scalar->new(name => 'Float'),
+                args => { ip => { type => GraphQL::Type::Scalar->new(name => 'String') } },
+                resolve => sub {
+                    my ($root, $args, $context, $info) = @_;
+                    return 22.5; # dummy temperatur
+                },
+            },
+            ports => {
+                type => GraphQL::Type::List->new(of => GraphQL::Type::Object->new(
+                    name => 'Port',
+                    fields => {
+                        name => { type => GraphQL::Type::Scalar->new(name => 'String') },
+                        status => { type => GraphQL::Type::Scalar->new(name => 'String') },
+                    }
+                )),
+                args => { ip => { type => GraphQL::Type::Scalar->new(name => 'String') } },
+                resolve => sub {
+                    my ($root, $args, $context, $info) = @_;
+                    return [
+                        { name => "eth0", status => "up" },
+                        { name => "eth1", status => "down" },
+                    ];
+                },
+            },
+        }
+    );
+
+    return GraphQL::Schema->new(query => $Query);
 }
 
 1;
-
-__DATA__
-
-GRAPHQL
-type Query {
-  hello: String
-}
-GRAPHQL
