0715a17c530b74cc5c53e6b262528a718aec0b38
Daniel Hansen <danielhansen1203@msn.com>
2025-05-01 00:48:21 +0200

    Test debugging

--- lib/MyApp/Schema/Graphql.pm
+++ lib/MyApp/Schema/Graphql.pm
@@ -1,14 +1,9 @@
-package MyApp::Schema::Graphql;
-use strict;
-use warnings;
-use GraphQL::Schema;
-
 sub graphql_schema {
     my ($class, $snmp_model) = @_;
 
     my $sdl = <<'EOF';
 type Query {
-  interfaceStatus(ip: String!, oid: String!): String
+  hello: String
 }
 EOF
 
@@ -16,13 +11,8 @@ EOF
         $sdl,
         {
             Query => {
-                interfaceStatus => sub {
-                    my ($root, $args) = @_;
-                    return $snmp_model->get_interface_status($args->{ip}, $args->{oid});
-                },
+                hello => sub { return "world" },
             },
         }
     );
 }
-
-1;
