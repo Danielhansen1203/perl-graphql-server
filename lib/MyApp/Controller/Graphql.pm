ff60507cfafff9d083d9daec3eccf0ced5753dad
Daniel Hansen <danielhansen1203@msn.com>
2025-05-02 21:16:45 +0200

    a

--- lib/MyApp/Controller/Graphql.pm
+++ lib/MyApp/Controller/Graphql.pm
@@ -13,30 +13,27 @@ sub execute {
 
     my $data = $c->req->json || {};
 
-    use Data::Dumper;
     $c->app->log->debug("RAW JSON data: " . Dumper($data));
 
     unless ($data->{query} && $data->{query} =~ /\S/) {
-        return $c->render(
-            status => 400,
-            json   => { error => 'Missing or empty GraphQL query' }
-        );
+        return $c->render(status => 400, json => { error => 'Missing or empty GraphQL query' });
     }
 
-    $c->app->log->debug("GraphQL query: " . ($data->{query} // '[undef]'));
+    my $opname = $data->{operationName};
+    $opname = undef unless defined($opname) && !ref($opname);  # <- fix her
 
-my $result = GraphQL::Execution::execute(
-    $schema->{schema},
-    $data->{query},
-    $data->{operationName} // undef,    # <--- tilføjet dette
-    undef,
-    $data->{variables} || {},
-    $schema->{root_value}
-);
-
+    my $result = GraphQL::Execution::execute(
+        $schema->{schema},
+        $data->{query},
+        $opname,
+        undef,
+        $data->{variables} || {},
+        $schema->{root_value}
+    );
 
     $c->render(json => $result);
 }
+
 
 
 
