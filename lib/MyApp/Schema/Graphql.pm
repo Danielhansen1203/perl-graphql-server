f61acfd6429a303faca7e3703bbb0473fe40c09b
Daniel Hansen <danielhansen1203@msn.com>
2025-05-02 21:37:01 +0200

    a

--- lib/MyApp/Schema/Graphql.pm
+++ lib/MyApp/Schema/Graphql.pm
@@ -4,7 +4,6 @@ use warnings;
 use GraphQL::Schema;
 
 sub build {
-    warn "Step 2";
     my ($snmp_model) = @_;
 
     my $sdl = <<'GRAPHQL';
@@ -13,18 +12,20 @@ type Query {
   hejAbkat: String
 }
 GRAPHQL
-
+warn "Steps 2";
     my $root_value = {
         GetSNMPInfo => sub {
             my ($args) = @_;
             return $snmp_model->get_snmp_info($args->{ip}, $args->{oid});
         },
         hejAbkat => sub {
+            warn "Steps 3";
             return "Hvis dette virker er det insane!";
         },
     };
 
     return {
+        warn "Steps 4";
         schema      => GraphQL::Schema->from_doc($sdl),
         root_value  => $root_value,
     };
