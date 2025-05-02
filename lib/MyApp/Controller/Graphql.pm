db53b38a0c4cfa2f79c66ace8392db810cd0d108
Daniel Hansen <danielhansen1203@msn.com>
2025-05-02 22:01:30 +0200

    a

--- lib/MyApp/Controller/Graphql.pm
+++ lib/MyApp/Controller/Graphql.pm
@@ -4,26 +4,34 @@ use MyApp::Schema::Graphql;
 use GraphQL::Execution;
 use Data::Dumper;
 
-sub execute {
-  my $c      = shift;
-  my $body   = $c->req->json;               # JSON-indhold fra klienten
-  my $query  = $body->{query};
-  my $vars   = $body->{variables} || {};    # eventuelle variabler
-  my $opname = $body->{operationName};      # operationName fra input
-  
-  warn "operationName: ", Dumper($opname);  # Debug: log værdien af operationName
-  
-  my $result = GraphQL::Execution->execute(
-      $schema,
-      $query,
-      $root_value,
-      undef,               # kontekst (f.eks. $c->req->headers eller undef)
-      $vars,
-      $opname,             # operationName parameteren
-      undef                # field resolver (valgfrit)
-  );
-  $c->render(json => $result);
-}
+my $compiled_schema;  # Holder på schema og root_value
 
+sub execute {
+    my $c = shift;
+
+    $compiled_schema ||= MyApp::Schema::Graphql::build($c->app->snmp_model);
+
+    my $schema     = $compiled_schema->{schema};
+    my $root_value = $compiled_schema->{root_value};
+
+    my $data = $c->req->json || {};
+
+    $c->app->log->debug("RAW JSON data: " . Dumper($data));
+
+    my $opname = $data->{operationName};
+    $opname = undef unless defined($opname) && !ref($opname);
+    warn "operationName: ", Dumper($opname);
+
+    my $result = GraphQL::Execution::execute(
+        $schema,
+        $data->{query},
+        $opname,
+        undef,
+        $data->{variables} || {},
+        $root_value
+    );
+
+    $c->render(json => $result);
+}
 
 1;
